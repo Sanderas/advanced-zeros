ccf3ec318236977b90f64c90aed00c29d5bb9507
Alexander <guts-817@rambler.ru>
2018-02-24 23:07:14 +0300

    my desicion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    // your implementation
+    var num = 0;
+    var arr = [];
+    for (var i = 2; i < base / 2; i++) {
+        for (var j = Math.floor(base / 2); j > i && i * j >= base; j--) {
+            if (i * j == base) {
+                arr.push([i, j]);
+            }
+        }
+    }
+
+
+    var milestone = (arr.length) ? arr[arr.length - 1][1] : base;
+
+    for (var i = milestone; i <= number; i += milestone) {
+        num++;
+        var j = i;
+        j /= milestone;
+        while (j % milestone == 0) {
+            j /= milestone;
+            num++;
+        }
+    }
+
+    return num;
 }
